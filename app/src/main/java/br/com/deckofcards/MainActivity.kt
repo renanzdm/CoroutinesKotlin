1f00b5807704c1512461e4cef9044e76c2f21bad
renanzdm <renanzdm@gmail.com>
2020-12-31 18:48:32 -0300

    Init Project Consumer DeckOfCards Api

---
+++ app/src/main/java/br/com/deckofcards/MainActivity.kt
@@ -0,0 +1,18 @@
+package br.com.deckofcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.deckofcards.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/br/com/deckofcards/model/Filme.kt
@@ -0,0 +1,6 @@
+package br.com.deckofcards.model
+
+data class Filme (
+    val id: Int,
+    val titulo: String
+)
